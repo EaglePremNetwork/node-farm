be0e20cbde002fa156c0cfa6f6366eb9598f16d0
Prem <prem.fcr@gmail.com>
2024-12-15 20:35:12 +0530

    remove localhost ip for PORT

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
+server.listen(8000, () => {
   console.log("listening to the server on port 8000");
 });
